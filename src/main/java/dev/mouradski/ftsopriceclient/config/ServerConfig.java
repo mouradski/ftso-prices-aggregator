2ba9110300a56275b97db634d8bf56495f7c1a9d
mouradski <mouradski@live.com>
2023-05-30 10:17:48 +0200

    Update ServerConfig.java

--- src/main/java/dev/mouradski/ftsopriceclient/config/ServerConfig.java
+++ src/main/java/dev/mouradski/ftsopriceclient/config/ServerConfig.java
@@ -6,24 +6,9 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration(proxyBeanMethods = false)
 public class ServerConfig {
-
-    /*
-    @Bean
-    public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
-        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
-        container.setMaxTextMessageBufferSize(3276800);
-        container.setMaxBinaryMessageBufferSize(3276800);
-        return container;
-    }
-
-     */
-
-
-
+    
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
-
-
 }
