4a8facf6dcdd067b01bc777834414c98283c56d3
mouradski <mouradski@live.com>
2023-06-07 23:56:46 +0200

    toobit exchange

---
+++ src/main/java/dev/mouradski/ftso/trades/client/toobit/ToobitClientEndpoint.java
@@ -0,0 +1,70 @@
+package dev.mouradski.ftso.trades.client.toobit;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import dev.mouradski.ftso.trades.client.AbstractClientEndpoint;
+import dev.mouradski.ftso.trades.model.Trade;
+import dev.mouradski.ftso.trades.service.PriceService;
+import dev.mouradski.ftso.trades.utils.SymbolHelper;
+import jakarta.websocket.ClientEndpoint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ClientEndpoint
+@Component
+public class ToobitClientEndpoint extends AbstractClientEndpoint {
+
+    protected ToobitClientEndpoint(PriceService priceSender, @Value("${exchanges}") List<String> exchanges, @Value("${assets}") List<String> assets) {
+        super(priceSender, exchanges, assets);
+    }
+
+    @Override
+    protected String getUri() {
+        return "wss://stream.toobit.com/quote/ws/v1";
+    }
+
+    @Override
+    protected void subscribe() {
+
+        var pairs = getAssets(true).stream().map(v -> v + "USDT").collect(Collectors.joining(","));
+        this.sendMessage("{\"symbol\": \"PAIRS\",\"topic\": \"trade\",\"event\": \"sub\",\"params\":{\"binary\": false}}".replace("PAIRS", pairs));
+    }
+
+    @Override
+    protected String getExchange() {
+        return "toobit";
+    }
+
+    @Override
+    protected List<Trade> mapTrade(String message) throws JsonProcessingException {
+
+        if (!message.contains("symbolName")) {
+            return new ArrayList<>();
+        }
+
+        var tradeMessage = this.objectMapper.readValue(message, TradeMessage.class);
+
+        var symbol = SymbolHelper.getSymbol(tradeMessage.getSymbolName());
+
+        var trades = new ArrayList<Trade>();
+
+        tradeMessage.getTrades().stream().sorted(Comparator.comparing(ToobitTrade::getTime)).forEach(toobitTrade -> {
+            trades.add(Trade.builder().exchange(getExchange()).symbol(symbol.getLeft()).quote(symbol.getRight())
+                    .price(toobitTrade.getPrice()).amount(toobitTrade.getQuantity()).build());
+        });
+
+        return trades;
+    }
+
+    @Scheduled(fixedDelay = 4 * 60 * 1000)
+    public void ping() {
+        this.sendMessage("{\"ping\": ID}".replace("ID", new Date().getTime() + ""));
+    }
+
+}
