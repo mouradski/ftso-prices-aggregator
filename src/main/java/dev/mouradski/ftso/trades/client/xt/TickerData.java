d07cd810e3af5a80be0db02d061ce3bde378fafe
mouradski <mouradski@live.com>
2023-12-26 19:34:32 +0100

    Fix compil

--- src/main/java/dev/mouradski/ftso/trades/client/xt/TickerData.java
+++ src/main/java/dev/mouradski/ftso/trades/client/xt/TickerData.java
@@ -1,7 +1,6 @@
 package dev.mouradski.ftso.trades.client.xt;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
