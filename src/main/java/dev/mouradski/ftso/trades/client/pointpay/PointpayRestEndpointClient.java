7e9147277dff7f0baaaf610b48b01f5a9817990f
mouradski <mouradski@live.com>
2024-01-31 23:08:57 +0100

    Pointpay

---
+++ src/main/java/dev/mouradski/ftso/trades/client/pointpay/PointpayRestEndpointClient.java
@@ -0,0 +1,54 @@
+package dev.mouradski.ftso.trades.client.pointpay;
+
+import dev.mouradski.ftso.trades.client.AbstractClientEndpoint;
+import dev.mouradski.ftso.trades.model.Ticker;
+import dev.mouradski.ftso.trades.utils.SymbolHelper;
+import io.quarkus.scheduler.Scheduled;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@ApplicationScoped
+public class PointpayRestEndpointClient extends AbstractClientEndpoint {
+    @Override
+    protected String getUri() {
+        return null;
+    }
+
+    @Override
+    protected String getExchange() {
+        return "pointpay";
+    }
+
+    @Scheduled(every = "3s")
+    public void getTickers() {
+        this.lastTickerTime = System.currentTimeMillis();
+
+        if (exchanges.contains(getExchange())) {
+            var request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.p2pb2b.com/api/v2/public/tickers"))
+                    .header("Content-Type", "application/json")
+                    .GET()
+                    .build();
+
+            try {
+                var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+                var tickerResponse = gson.fromJson(response.body(), TickersResponse.class);
+
+                tickerResponse.getResult().forEach((key, value) -> {
+                    var pair = SymbolHelper.getPair(key);
+
+                    if (getAssets(true).contains(pair.getLeft()) && getAllQuotesExceptBusd(true).contains(pair.getRight())) {
+                        pushTicker(Ticker.builder().exchange(getExchange()).base(pair.getLeft()).quote(pair.getRight()).lastPrice(Double.valueOf(value.getTicker().getLast())).timestamp(currentTimestamp()).build());
+                    }
+                });
+
+            } catch (IOException | InterruptedException e) {
+            }
+        }
+    }
+}
