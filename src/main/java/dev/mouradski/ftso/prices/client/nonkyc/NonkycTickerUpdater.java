845a2ac5677a6b6306c5bdd4650f61d2a9a086a8
mouradski <mouradski@live.com>
2024-03-06 11:18:19 +0100

    NonKyc Exchange

---
+++ src/main/java/dev/mouradski/ftso/prices/client/nonkyc/NonkycTickerUpdater.java
@@ -0,0 +1,70 @@
+package dev.mouradski.ftso.prices.client.nonkyc;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import dev.mouradski.ftso.prices.client.AbstractClientEndpoint;
+import dev.mouradski.ftso.prices.model.Source;
+import dev.mouradski.ftso.prices.model.Ticker;
+import io.quarkus.scheduler.Scheduled;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Arrays;
+
+@ApplicationScoped
+public class NonkycTickerUpdater extends AbstractClientEndpoint {
+
+    @Scheduled(every = "1s")
+    public void getTickers() {
+
+        if (!exchanges.contains(getExchange())) {
+            return;
+        }
+
+        this.lastTickerTime = System.currentTimeMillis();
+
+        Arrays.asList("https://api.nonkyc.io/api/v2/tickers").forEach(url -> {
+            var request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Content-Type", "application/json")
+                    .GET()
+                    .build();
+
+            Uni.createFrom().completionStage(() -> client.sendAsync(request, HttpResponse.BodyHandlers.ofString()))
+                    .onItem().transform(response -> {
+                        try {
+                            return objectMapper.readValue(response.body(), TickerData[].class);
+                        } catch (JsonProcessingException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .onItem().transformToMulti(tickersResponse -> Multi.createFrom().iterable(Arrays.stream(tickersResponse).toList()))
+                    .subscribe().with(ticker -> {
+                        if (getAssets(true).contains(ticker.getBase()) && getAllQuotesExceptBusd(true).contains(ticker.getQuote())) {
+                            pushTicker(Ticker.builder()
+                                    .source(Source.REST)
+                                    .exchange(getExchange())
+                                    .base(ticker.getBase())
+                                    .quote(ticker.getQuote())
+                                    .lastPrice(Double.parseDouble(ticker.getLastPrice()))
+                                    .timestamp(currentTimestamp())
+                                    .build());
+                        }
+                    }, failure -> {});
+        });
+    }
+
+    @Override
+    protected String getUri() {
+        return null;
+    }
+
+    @Override
+    protected String getExchange() {
+        return "nonkyc";
+    }
+}
---
+++ src/main/java/dev/mouradski/ftso/prices/client/nonkyc/TickerData.java
@@ -0,0 +1,19 @@
+package dev.mouradski.ftso.prices.client.nonkyc;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TickerData {
+    @JsonProperty("base_currency")
+    private String base;
+    @JsonProperty("target_currency")
+    private String quote;
+    @JsonProperty("last_price")
+    private String lastPrice;
+
+}
