ae9980e4e1be45c5dff736aa37048ace6c39c591
mouradski <mouradski@live.com>
2024-10-27 08:44:41 +0100

    fix azbit

--- src/main/java/dev/mouradski/ftso/prices/client/azbit/AzbitClientEndpoint.java
+++ src/main/java/dev/mouradski/ftso/prices/client/azbit/AzbitClientEndpoint.java
@@ -26,12 +26,50 @@ public class AzbitClientEndpoint extends AbstractClientEndpoint {
     }
 
     @Override
-    protected String getUri() {
-        return "wss://ws.azbit.com/latest-price";
+    protected String getExchange() {
+        return "azbit";
     }
 
     @Override
-    protected String getExchange() {
-        return "azbit";
+    protected String getUri() {
+        return null;
+    }
+
+    @Scheduled(every = "3s")
+    public void getTickers() {
+        this.messageReceived();
+        if (exchanges.contains(getExchange()) && this.isCircuitClosed()) {
+            var request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://data.azbit.com/api/tickers"))
+                    .header("Content-Type", "application/json")
+                    .GET()
+                    .build();
+
+            Uni.createFrom().completionStage(() -> client.sendAsync(request, HttpResponse.BodyHandlers.ofString()))
+                    .onItem().transform(response -> {
+                        try {
+                            return objectMapper.readValue(response.body(), TickerData[].class);
+                        } catch (JsonProcessingException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .onItem().transformToMulti(tickerResponse -> Multi.createFrom().iterable(Arrays.stream(tickerResponse).toList()))
+                    .onFailure().invoke(this::catchRestError)
+                    .subscribe().with(tickerEntry -> {
+                        if (tickerEntry.getSymbol() != null && tickerEntry.getPrice() != null && tickerEntry.getPrice() != 0) {
+                            var pair = SymbolHelper.getPair(tickerEntry.getSymbol());
+                            if (getAssets(true).contains(pair.getLeft()) && getAllQuotes(true).contains(pair.getRight())) {
+                                pushTicker(Ticker.builder()
+                                        .source(Source.REST)
+                                        .exchange(getExchange())
+                                        .base(pair.getLeft())
+                                        .quote(pair.getRight())
+                                        .lastPrice(tickerEntry.getPrice())
+                                        .timestamp(currentTimestamp())
+                                        .build());
+                            }
+                        }
+                    }, this::catchRestError);
+        }
     }
 }
